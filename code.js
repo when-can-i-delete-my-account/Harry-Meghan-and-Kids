941d6576ec7e4cb9471b67220ab9216688d0a2be
when-can-i-delete-my-account <99968506+when-can-i-delete-my-account@users.noreply.github.com>
2023-02-11 11:30:12 +0000

    Add files via upload

--- code.js
+++ code.js
@@ -1,18 +1,13 @@
-Webcam.set({
-    width:350,
-    height:300,
-    image_format:'png',
-    png_quality:90
-});
-
-camera = document.getElementById("camera");
-
 Webcam.attach( '#camera' );
 
-function modelLoaded()
-{
-    console.log('model loaded!');
-}
+camera = document.getElementById("camera");
+      
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
 
 function take_snapshot()
 {
@@ -21,6 +16,33 @@ function take_snapshot()
     });
 }
 
-console.log('ml5 version:', ml5.version);
+  console.log('ml5 version:', ml5.version);
+  
+  // Initialize the Image Classifier method with MobileNet
+//classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JACkTMciY/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/_HMsmoXPo/model.json',modelLoaded);
 
-classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/_HMsmoXPo/model.json', modelLoaded);
+  // When the model is loaded
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+// A function to run when we get any errors and the results
+function gotResult(error, results) {
+  // Display error in the console
+  if (error) {
+    console.error(error);
+  } else {
+    // The results are in an array ordered by confidence.
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
